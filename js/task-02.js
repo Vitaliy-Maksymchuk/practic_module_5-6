f98e1b5c6092f257d676f3adc9c7bb62dc46922a
SvitlanaPro <128604473+SvitlanaPro@users.noreply.github.com>
2024-10-27 20:04:59 +0200

    complete

--- js/task-02.js
+++ js/task-02.js
@@ -3,3 +3,20 @@
 // Функція створює об"єкт товару, додає йому унікальний ідентифікатор у властивість id та викликає callback,
 // передаючи йому створений об"єкт
 // showProduct(product)  - колбек, що приймає об"єкт продукта та виводить його у консоль
+
+const makeProduct = (name, price, callback)=>{
+    const product = {
+        id:Math.random(),
+        name,
+        price,
+    }
+    callback(product);
+  }
+  const showProduct = (newProduct)=>console.log(newProduct);
+
+const showProductSecond = (ProductSecond)=>console.log(ProductSecond);
+  
+  makeProduct("Orange",60,showProduct);
+
+makeProduct("Green",60,showProductSecond);
+  
