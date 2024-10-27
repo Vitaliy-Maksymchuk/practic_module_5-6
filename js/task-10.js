d2df1c7dd135df322d5be89d7d4a63fea73ecb9b
ginny <ir.gonccharova.up@gmail.com>
2024-10-27 20:37:39 +0200

    complete

--- js/task-10.js
+++ js/task-10.js
@@ -3,12 +3,39 @@
 // Додай класу статичну властивість Priopity,
 //     в якому буде зберігатися об"єкт з пріоритетами
 //     Додай методи addNote(note), removeNote(text), updatePriority(text, newPriority)
+class Notes {
+static PRIORITY = {
+LOW: "low",
+HIGHT: "hight",
+}
 
+constructor() {
+    this.items = []
+}
+
+addNote(note) {
+    this.items.push(note)
+}
+    
+removeNote(text) {
+    this.items = this.items.filter(item => item.text !== text)
+}
+
+updatePriority(text, newPriority) {
+    this.items = this.items.map(item => {
+        if(item.text === text) {
+            item.priority = newPriority
+        } 
+        return item;
+    })
+}
+}
 const note1 = new Notes()
 
 note1.addNote({ text: 'Note1', priority: Notes.PRIORITY.LOW })
 note1.addNote({ text: 'Note2', priority: Notes.PRIORITY.LOW })
 note1.addNote({ text: 'Note3', priority: Notes.PRIORITY.LOW })
+console.log(note1.items)
 note1.removeNote('Note1')
 note1.updatePriority('Note2', Notes.PRIORITY.HIGHT)
 console.log(note1.items)
