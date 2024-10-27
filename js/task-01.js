2a47e27ef359bacf09de493fd9c68b51b15f11b1
Vitaliy Maksymchuk <vitalii.v.maksymchuk@gmail.com>
2024-10-27 19:28:49 +0200

    task-1_done

--- js/task-01.js
+++ js/task-01.js
@@ -2,3 +2,15 @@
 // letMeSeeYourName(callback) - запитує ім"я користувача через prompt та викликає callback функцію
 // greet(name) - колбек, що приймає ім"я та виводить у консоль рядок "Привіт < name > "
 // Потрібно реалізувати перевірку, що prompt не порожній
+
+const letMeSeeYourName = (callback) => {
+    const message = prompt('ВВедіть ім\'я');
+    if(!message) {
+        return 'ok';    
+    }
+    callback(message);
+} 
+
+const greet = (name) => console.log(`Привіт ${name}  `);
+
+letMeSeeYourName(greet)
